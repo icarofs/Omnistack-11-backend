637b6cad3f37c09117e23be6c59ea76f55899aeb
Ícaro Ferreira de Souza <icaroferreira.souza@gmail.com>
2020-03-24 16:36:09 -0300

    create and list incidents

--- src/routes.js
+++ src/routes.js
@@ -2,9 +2,13 @@ const express = require("express");
 const routes = express.Router();
 
 const OngController = require("./controllers/OngController");
+const IncidentController = require("./controllers/IncidentController");
 
 routes.get("/ongs", OngController.index);
 
 routes.post("/ongs", OngController.create);
+
+routes.get("/incidents", IncidentController.index);
+routes.post("/incidents", IncidentController.create);
 
 module.exports = routes;
